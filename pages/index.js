f30a488e2605e3953d06a7fb1fb47a6dd450903d
Kyle Lemire <hello@klemire.com>
2021-04-26 21:10:38 -0500

    finished layout of movie results

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -9,6 +12,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <Nav />
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { genre } = context.query;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || request.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
