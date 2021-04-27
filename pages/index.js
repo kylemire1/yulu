386393af1a03f5bd5d9bec90f876024654c98d46
Kyle Lemire <hello@klemire.com>
2021-04-26 21:43:16 -0500

    updated page title

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home({ results }) {
   return (
     <div>
       <Head>
-        <title>Hulu</title>
+        <title>Yulu</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
